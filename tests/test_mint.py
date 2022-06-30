3fc2323f7f58ce5663650ca3405874cbd2b62a06
Chan-Ho Suh <chanhosuh@gmail.com>
2022-06-29 21:14:08 -0400

    Add test for approval in upgrade

--- tests/test_mint.py
+++ tests/test_mint.py
@@ -6,6 +6,12 @@ def test_initial_balance_empty(token, v1_hodler):
     assert token.balanceOf(v1_hodler) == 0
 
 
+def test_cannot_upgrade_without_approval(token, v1_hodler, v1_token):
+    v1_token.approve(token, 0, {"from": v1_hodler})
+    with brownie.reverts("No Approval"):
+        token.upgrade(v1_hodler, {"from": v1_hodler})
+
+
 def test_holders_of_v1_can_upgrade(token, v1_token, v1_hodler):
     token.upgrade(v1_hodler, {"from": v1_hodler})
     assert token.balanceOf(v1_hodler) > 0
@@ -32,3 +38,4 @@ def test_cannot_upgrade_twice(token, v1_hodler, v1_token):
     token.upgrade(v1_hodler, {"from": v1_hodler})
     with brownie.reverts("No balance"):
         token.upgrade(v1_hodler, {"from": v1_hodler})
+
